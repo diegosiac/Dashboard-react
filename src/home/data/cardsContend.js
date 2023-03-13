1fb37666974abe97d0d6b9ad39b9a1dcf36e71bb
DiegoADC <diego35502@hotmail.com>
2023-03-12 20:22:33 -0600

    debug img Home

--- src/home/data/cardsContend.js
+++ src/home/data/cardsContend.js
@@ -2,25 +2,25 @@ export const cardsData = [
     {
         title: 'ANALÍTICAS FÁCILES',
         description:'¡BUM TRAKER rastrea las estadísticas de los usuarios de Twitch! ', 
-        url:'../../../assets/imgs/task-3.png', 
+        url:'./assets/imgs/task-3.png', 
         nameImg:'blackboard with graph'
     },
     {
         title: '¡ANÁLISIS SIMPLIFICADO AL ALCANCE DE SU MANO!',
         description:'Obtenga una comprensión más profunda del crecimiento y las tendencias.', 
-        url:'../../../assets/imgs/ebay.png', 
+        url:'./assets/imgs/ebay.png', 
         nameImg:'Ibai Twitch photo'
     },
     {
         title: '¡LO TENEMOS CUBIERTO!',
         description:'Siempre buscamos la mejor experiencia para nuestros usuarios.', 
-        url:'../../../assets/imgs/srfufi.jpg', 
+        url:'./assets/imgs/srfufi.jpg', 
         nameImg:'Fufi Twitch photo'
     },
     {
         title: 'ACCESO FÁCIL DATOS DE TWITCH',
         description:'Busca streamer populares de Twitch.', 
-        url:'../../../assets/imgs/task-2.png', 
+        url:'./assets/imgs/task-2.png', 
         nameImg:'magnifying glass'
     },
 ];
